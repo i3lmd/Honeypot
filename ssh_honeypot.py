ee66c3b83c4d2e5a985a3e72113bf28d09271df0
Ahmad <ahmadsab8@gmail.com>
2025-05-01 02:24:12 +0800

    adding the initial emulate_shell function

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -1,39 +1,54 @@
-#libraries 
+# libraries
 import logging
 from logging.handlers import RotatingFileHandler
 import socket
 
 # Constants
-logging_format=logging.Formatter('%(message)s')
+logging_format = logging.Formatter('%(message)s')
 
 # Loggers & Logging files
-funnel_logger=logging.getLogger('Funnel_Logger')
+funnel_logger = logging.getLogger('Funnel_Logger')
 funnel_logger.setLevel(logging.INFO)
-funnel_handler=RotatingFileHandler('audits.log', maxBytes=2000, backupCount=5)
+funnel_handler = RotatingFileHandler(
+    'audits.log', maxBytes=2000, backupCount=5)
 funnel_handler.setFormatter(logging_format)
 funnel_logger.addHandler(funnel_handler)
 
-credentials_logger=logging.getLogger('Credential_Logger')
+credentials_logger = logging.getLogger('Credential_Logger')
 credentials_logger.setLevel(logging.INFO)
-credentials_handler=RotatingFileHandler('cmd_audits.log', maxBytes=2000, backupCount=5)
+credentials_handler = RotatingFileHandler(
+    'cmd_audits.log', maxBytes=2000, backupCount=5)
 credentials_handler.setFormatter(logging_format)
 credentials_logger.addHandler(credentials_handler)
 
 # Emulate Shell
+
 
 def emulate_shell(channel, client_ip):
     """
     Way to send dialogue messages/strings over the SSH connection
     """
     # Log the command
-    channel.send(b'corporate-jumpbox2$')
-    command=b""
+    channel.send("user@honeypot$ ")
+    command = b""
     while True:
-        char=channel.recv(1)
+        char = channel.recv(1)
         channel.send(char)
         if not char:
             channel.close()
-        
+        command += char
+
+        if char == b"\r":
+            if command.strip() == b"whoami":
+                response = b"\n" + "user" + b"\r\n"
+            elif command.strip() == b"exit":
+                response = b"\n" + "Goodbye!" + b"\n"
+                channel.close()
+        channel.send(response)
+        channel.send(b"user@honeypot$ ")
+        command = b""
+
+
 # SSH Server + Sockets
 
 # Provision SSH_based  Server
